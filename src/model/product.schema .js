efe087be1e2c772c5d169dbd8ef0dfda335f26d1
rayan2228 <rayanh.dev@gmail.com>
2024-12-26 20:51:00 +0600

    product inventory

--- src/model/product.schema .js
+++ src/model/product.schema .js
@@ -6,11 +6,13 @@ const productSchema = new Schema({
     description: { type: String },
     category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
     brand: { type: String },
+    thumbnail: { publicId: String, url: String },
     images: [{
         publicId: String,
         url: String
     }],
     variants: [{ type: mongoose.Schema.Types.ObjectId, ref: "ProductVariant" }],
+    isActive: { type: Boolean, default: true },
 }, { timestamps: true });
 
 export const Product = models.Product || model("Product", productSchema);
---
+++ src/model/productInventory.schema .js
@@ -0,0 +1,15 @@
+import { model, models, Schema } from "mongoose";
+
+const productInventorySchema = new Schema({
+    product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    variant: { type: mongoose.Schema.Types.ObjectId, ref: "ProductVariant", required: true },
+    purchasePrice: { type: Number, required: true },
+    sellingPrice: { type: Number, required: true },
+    discount: { discountType: String, discountValue: Number },
+    stock: { type: Number, default: 0 },
+    inStock: { type: Boolean, default: false },
+    sku: { type: String, unique: true, required: true },
+    images: [String],
+});
+
+export const ProductInventory = models.ProductInventory || model("ProductInventory", productInventorySchema);
