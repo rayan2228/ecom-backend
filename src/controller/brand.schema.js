e62957278cb86b7400e4c3c1eb1aa9143a47fc11
rayan2228 <rayanh.dev@gmail.com>
2025-01-09 20:32:58 +0600

    delete brand and update brand

--- src/controller/brand.schema.js
+++ src/controller/brand.schema.js
@@ -1,4 +1,5 @@
 import { Brand } from "../model/brand.schema"
+import { cloudinaryDelete, cloudinaryUpload } from "../service/cloudinary"
 import { TryCatch } from "../utils/TryCatch"
 
 const getBrands = TryCatch(async (req, res) => {
@@ -39,4 +40,47 @@ const createBrand = TryCatch(async (req, res) => {
     return res.status(201).json(new ApiSuccess(201, "brand created successfully", { brand }))
 })
 
-export { createBrand, getBrands, getBrand }
+const updateBrand = TryCatch(async (req, res) => {
+    let { name, slug, description, isActive } = req.body
+    const brand = await Brand.findOne({ name: req.params.name })
+    if (!brand) {
+        throw new ApiError(404, "brand not found")
+    }
+    if (!name) {
+        throw new ApiError(400, "name is required")
+    }
+    if (!slug) {
+        slug = name.replaceAll(" ", "-").toLowerCase()
+    }
+    if (req.file) {
+        if (brand.thumbnail.public_id) {
+            await cloudinaryDelete(brand.thumbnail.public_id)
+        }
+        const thumbnail = req.file
+        const cloudinaryResult = await cloudinaryUpload(thumbnail.path, name, "brand")
+        brand.thumbnail = {
+            public_id: cloudinaryResult.public_id,
+            url: cloudinaryResult.optimizeUrl,
+        }
+    }
+    brand.name = name
+    brand.slug = slug
+    brand.description = description || ""
+    brand.isActive = isActive || true
+    await brand.save()
+    return res.json(new ApiSuccess(200, "brand updated successfully", { brand }))
+})
+
+
+const deleteBrand = TryCatch(async (req, res) => {
+    const brand = await Brand.findOne({ name: req.params.name })
+    if (!brand) {
+        throw new ApiError(404, "brand not found")
+    }
+    if (brand.thumbnail.public_id) {
+        await cloudinaryDelete(brand.thumbnail.public_id)
+    }
+    await brand.deleteOne()
+    return res.json(new ApiSuccess(200, "brand deleted successfully", {}))
+})
+export { createBrand, getBrands, getBrand, updateBrand, deleteBrand }
