5b766224f0499747bdd6ed60fbcb8b1cf4a7d42a
rayan2228 <rayanh.dev@gmail.com>
2025-01-30 21:36:37 +0600

    remove createPermission function and update permissions route to include authentication and access checks

--- src/controller/permission.controller.js
+++ src/controller/permission.controller.js
@@ -1,18 +1,6 @@
 import { Permission } from "../model/permissionSchema.model.js";
 import { ApiError } from "../utils/ApiErrors.js";
 import { ApiSuccess } from "../utils/ApiSuccess.js";
-const createPermission = async (req, res) => {
-    const { name, description } = req.body;
-    if (!name) {
-        throw new ApiError(400, "name is required");
-    }
-    const newPermission = new Permission({
-        name,
-        description
-    });
-    await newPermission.save();
-    return res.json(new ApiSuccess(200, "permission created successfully", { newPermission }));
-}
 
 const getPermissions = async (req, res) => {
     const permissions = await Permission.find();
--- src/route/permission.route.js
+++ src/route/permission.route.js
@@ -1,11 +1,12 @@
-import e from "express"
-import { auth } from "../middleware/auth.middleware.js"
-import { checkAccess } from "../middleware/checkAccess.middleware.js"
-import { createPermission, getPermissions } from "../controller/permission.controller.js"
+import e from "express";
+import { getPermissions } from "../controller/permission.controller.js";
+import { auth } from "../middleware/auth.middleware.js";
+import { checkAccess } from "../middleware/checkAccess.middleware.js";
 
-const router = e.Router()
+const router = e.Router();
 
-router.route("/permissions").get(getPermissions).post(createPermission)
+router
+  .route("/permissions")
+  .get(auth, checkAccess(["view_users"]), getPermissions);
 
-
-export default router
+export default router;
