8af5e47366984fbfe06f31df624d49540cb9a04e
rayan2228 <rayanh.dev@gmail.com>
2025-01-30 21:29:44 +0600

    refactor checkAccess middleware to remove role checks and simplify permission validation

--- src/middleware/checkAccess.middleware.js
+++ src/middleware/checkAccess.middleware.js
@@ -1,20 +1,21 @@
-
-import { TryCatch } from "../utils/TryCatch.js";
 import { ApiError } from "../utils/ApiErrors.js";
-import { verifyTokenAndGetUser } from "../utils/middlewareUtils.js";
+import { TryCatch } from "../utils/TryCatch.js";
 
-export const checkAccess = function (roles = [], permissions = []) {
-    return TryCatch(async (req, res, next) => {
-        const user = req.user
-        const userRoles = user.roles.map(role => role.name)
-        const userPermissions = user.roles.map(role => role.permissions).flat().map(permission => permission.name)
+export const checkAccess = function (permissions = []) {
+  return TryCatch(async (req, res, next) => {
+    const user = req.user;
+    const userPermissions = user.roles
+      .map((role) => role.permissions)
+      .flat()
+      .map((permission) => permission.name);
 
-        const hasRole = roles.some(role => userRoles.includes(role))
-        const hasPermission = permissions.some(permission => userPermissions.includes(permission))
+    const hasPermission = permissions.some((permission) =>
+      userPermissions.includes(permission)
+    );
 
-        if (!hasRole || !hasPermission) {
-            throw new ApiError(403, "Unauthorized access")
-        }
-        next()
-    })
-}
+    if (!hasPermission) {
+      throw new ApiError(403, "Unauthorized access");
+    }
+    next();
+  });
+};
