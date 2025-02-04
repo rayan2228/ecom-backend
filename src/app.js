eeea2c0c1b507196c5cebf0aa8327eaa79772b48
rayan2228 <rayanh.dev@gmail.com>
2025-02-04 21:24:57 +0600

    feat: add routes and controllers for cart, compare, order, user details, and wishlist

--- src/app.js
+++ src/app.js
@@ -1,45 +1,56 @@
-import express from "express";
-import cors from "cors"
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
 import { origin } from "./constant.js";
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.static("public"))
-app.use(cors({
-    origin
-}))
-app.use(cookieParser())
-
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static("public"));
+app.use(
+  cors({
+    origin,
+  })
+);
+app.use(cookieParser());
 
 import { errorHandler } from "./middleware/errorHandler.middleware.js";
-import userRouter from "./route/user.route.js"
-import categoryRouter from "./route/category.route.js"
-import subCategoryRouter from "./route/subcategory.route.js"
-import brandRouter from "./route/brand.route.js"
-import productRouter from "./route/product.route.js"
-import couponRouter from "./route/coupon.route.js"
-import permissionRouter from "./route/permission.route.js"
-import roleRouter from "./route/role.route.js"
-app.use("/api/v1", userRouter)
-app.use("/api/v1", categoryRouter)
-app.use("/api/v1", subCategoryRouter)
-app.use("/api/v1", brandRouter)
-app.use("/api/v1", productRouter)
-app.use("/api/v1", couponRouter)
-app.use("/api/v1", permissionRouter)
-app.use("/api/v1", roleRouter)
+import brandRouter from "./route/brand.route.js";
+import cartRouter from "./route/cart.route.js";
+import categoryRouter from "./route/category.route.js";
+import compareRouter from "./route/compare.route.js";
+import couponRouter from "./route/coupon.route.js";
+import orderRouter from "./route/order.route.js";
+import permissionRouter from "./route/permission.route.js";
+import productRouter from "./route/product.route.js";
+import roleRouter from "./route/role.route.js";
+import subCategoryRouter from "./route/subcategory.route.js";
+import userRouter from "./route/user.route.js";
+import userDetailsRouter from "./route/userDetails.route.js";
+import wishlistRouter from "./route/wishlist.route.js";
+app.use("/api/v1", userRouter);
+app.use("/api/v1", categoryRouter);
+app.use("/api/v1", subCategoryRouter);
+app.use("/api/v1", brandRouter);
+app.use("/api/v1", productRouter);
+app.use("/api/v1", couponRouter);
+app.use("/api/v1", permissionRouter);
+app.use("/api/v1", roleRouter);
+app.use("/api/v1", wishlistRouter);
+app.use("/api/v1", compareRouter);
+app.use("/api/v1", orderRouter);
+app.use("/api/v1", cartRouter);
+app.use("/api/v1", userDetailsRouter);
 
-
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.get("/test", (_, res) => {
-    res.json({ message: "hello world" })
-})
+  res.json({ message: "hello world" });
+});
 
 app.all("*", (_, res) => {
-    res.json({ message: "not found" })
-})
+  res.json({ message: "not found" });
+});
 
-export { app }
+export { app };
+
